d6c42fc051cc5ac569d92f2c36509e7c56ec1b14
priyashan1 <priyashan@xgengroup.com.au>
2025-04-02 14:19:49 +0530

    Manifest update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  build: {
-    manifest: true,
-  },
   plugins: [
     react(),
     VitePWA({
