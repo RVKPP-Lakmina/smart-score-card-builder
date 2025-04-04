cc14e8af9c30bc6bc43d217c59c96a1993bd1c77
priyashan1 <priyashan@xgengroup.com.au>
2025-04-04 17:35:44 +0530

    grill down score card layout

--- src/components/ui/EditableText.tsx
+++ src/components/ui/EditableText.tsx
@@ -10,18 +10,21 @@ const EditableText: React.FC<EditableTextProps> = ({
   onValueChange,
 }: EditableTextProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [text, setText] = useState(label);
-  const [isFocused, setIsFocused] = useState(false);
+  const [text] = useState(label);
+  // const [isFocused, setIsFocused] = useState(false);
 
   const handleDoubleClick = () => {
     setIsEditing(true);
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(event.target.value || 0.0);
+    if (value > 1) event.target.value = "1.00";
+    if (value < 0) event.target.value = "0.00";
+    onValueChange(event.target.value);
+  };
 
   const handleBlur = () => {
-    const value = Number(event.target.value || 0.0);
-
     onValueChange(text);
     setIsEditing(false);
   };
